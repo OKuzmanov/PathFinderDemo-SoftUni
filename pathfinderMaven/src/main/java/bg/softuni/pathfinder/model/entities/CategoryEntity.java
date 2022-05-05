1ad9a2926d7a925909535c51ab9f7c670105264f
OKuzmanov <88836482+OKuzmanov@users.noreply.github.com>
2022-05-05 13:00:35 +0300

    Pathfinder build with Maven

---
+++ pathfinderMaven/src/main/java/bg/softuni/pathfinder/model/entities/CategoryEntity.java
@@ -0,0 +1,39 @@
+package bg.softuni.pathfinder.model.entities;
+
+import bg.softuni.pathfinder.model.enums.CategoryEnum;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class CategoryEntity extends BaseEntity{
+
+    @Enumerated
+    @Column(nullable = false, unique = true)
+    private CategoryEnum name;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    public CategoryEntity() {
+    }
+
+    public CategoryEnum getName() {
+        return name;
+    }
+
+    public void setName(CategoryEnum name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
